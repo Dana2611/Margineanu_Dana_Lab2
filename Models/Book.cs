f1bd04ae40a407dd6d4af696a78c9a671510b7e1
Dana2611 <dana.margineanu3@yahoo.com>
2024-11-15 18:26:02 +0200

    Laborator 3

--- Models/Book.cs
+++ Models/Book.cs
@@ -21,5 +21,7 @@ namespace Margineanu_Dana_Lab2.Models
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     } //navigation property
 }
